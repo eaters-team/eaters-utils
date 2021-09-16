37f7ff41598d4fe573f9bf498659359bbd795f45
Nicolas Du <nicolas.duvollet@domicours.fr>
2021-09-16 10:40:19 +0200

    Add missing default on icons require

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ export const b64 = require('./utils/b64')
 export const chunker = require('./utils/chunker')
 export const encryption = require('./utils/encryption')
 
-export const VueIcons = require('./vue/components/icons/icons')
+export const VueIcons = require('./vue/components/icons/icons').default
